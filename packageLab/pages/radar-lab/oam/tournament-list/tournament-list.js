e3a84afcec3a72e1ba6b035af1f4ea86541ef252
william <william.zan@gmail.com>
2026-08-11 13:48:45 +0800

    fix: 修复 packageLab 分组内 tournament-list.js 跨分包引用服务的路径错误

--- packageLab/pages/radar-lab/oam/tournament-list/tournament-list.js
+++ packageLab/pages/radar-lab/oam/tournament-list/tournament-list.js
@@ -1,7 +1,6 @@
 const { ensureRadarLabAccess } = require('../../../../utils/radar-access.js');
-const { fetchTournamentList } = require('../../../../services/radar-api.js');
+const { fetchTournamentList, oamUpsert } = require('../../../../services/radar-api.js');
 const { getRadarListScope } = require('../../../../utils/radar-list-scope.js');
-const { oamUpsert } = require('../../../../services/tournament-api.js');
 
 /**
  * 解析 CSV 格式的球队初始基准积分文本
